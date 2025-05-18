d68338373541689cd8cf39f92c16040b1959c612
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-05-18 14:58:45 +0530

    Revert "sm6450-common: Work around using prebuilt audio primary HAL"
    This reverts commit 44ca7fa2cd0002e2b53cdc766ca09028f5e9e091.

--- common.mk
+++ common.mk
@@ -72,6 +72,7 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.3-impl \
     audioadsprpcd \
     audio.bluetooth.default \
+    audio.primary.parrot \
     audio.r_submix.default \
     audio.usb.default \
     libqcomvisualizer \
