0b7d5943b6c34e1ca4dda98c415d9ce07cd0ffad
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-06-28 19:47:07 +0530

    sm6450-common: Add missing media configs

--- common.mk
+++ common.mk
@@ -204,9 +204,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/ravelin/media_profiles_ravelin.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_ravelin.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/parrot/media_codecs_performance_parrot_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_parrot_v1.xml \
-    $(LOCAL_PATH)/media/parrot/media_codecs_parrot_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_parrot_v1.xml \
-    $(LOCAL_PATH)/media/parrot/media_profiles_parrot_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_parrot_v1.xml
+    $(LOCAL_PATH)/media/parrot/v0/media_codecs_performance_parrot_v0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_parrot_v1.xml \
+    $(LOCAL_PATH)/media/parrot/v0/media_codecs_parrot_v0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_parrot_v1.xml \
+    $(LOCAL_PATH)/media/parrot/v0/media_profiles_parrot_v0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_parrot_v1.xml \
+    $(LOCAL_PATH)/media/parrot/v1/media_codecs_performance_parrot_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_parrot_v1.xml \
+    $(LOCAL_PATH)/media/parrot/v1/media_codecs_parrot_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_parrot_v1.xml \
+    $(LOCAL_PATH)/media/parrot/v1/media_profiles_parrot_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_parrot_v1.xml \
+    $(LOCAL_PATH)/media/parrot/v2/media_codecs_performance_parrot_v2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_parrot_v1.xml \
+    $(LOCAL_PATH)/media/parrot/v2/media_codecs_parrot_v2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_parrot_v1.xml \
+    $(LOCAL_PATH)/media/parrot/v2/media_profiles_parrot_v2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_parrot_v1.xml
 
 PRODUCT_PACKAGES += \
     libpalclient
