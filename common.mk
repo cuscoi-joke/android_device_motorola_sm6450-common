6e78adbfd1111c38c55b3edf01d2d6f794e36950
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-06-28 20:21:43 +0530

    sm6450-common: fixup! audio_effects.xml copy to vendor

--- common.mk
+++ common.mk
@@ -85,7 +85,7 @@ PRODUCT_PACKAGES += \
     sound_trigger.primary.parrot
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/sku_parrot/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_parrot/audio_effects.conf \
+    $(LOCAL_PATH)/audio/sku_parrot/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_parrot/audio_effects.xml \
     $(LOCAL_PATH)/audio/sku_parrot/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_parrot/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/sku_parrot/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_parrot/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/sku_parrot/mixer_paths_parrot_qrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_parrot/mixer_paths_parrot_qrd.xml \
