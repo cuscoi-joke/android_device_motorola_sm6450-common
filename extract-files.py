b61dd6dc73a8b228d182ae162ebc709372111c1e
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-07-08 21:37:56 +0530

    sm6450-common: Import QCC blobs

--- extract-files.py
+++ extract-files.py
@@ -37,6 +37,9 @@ lib_fixups: lib_fixups_user_type = {
     (
         'vendor.qti.imsrtpservice@3.0',
         'vendor.qti.diaghal@1.0',
+        'vendor.qti.hardware.qccsyshal@1.0',
+        'vendor.qti.hardware.qccsyshal@1.1',
+        'vendor.qti.hardware.qccvndhal@1.0',
         'com.qualcomm.qti.dpm.api@1.0',
         'vendor.qti.hardware.dpmservice@1.0',
     ): lib_fixup_vendor_suffix,
