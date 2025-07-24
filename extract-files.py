63c025f3c315788eefb4099b33e1a14aa863a172
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-07-24 23:40:01 +0530

    sm6450-common: Update ims patches
    https://github.com/moto-sm6450-dev/ims
    
    Signed-off-by: Rakhshan7070 <alirakhshan7070@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -88,6 +88,9 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
+    'system_ext/priv-app/ims/ims.apk': blob_fixup().apktool_patch(
+        'ims-patches'
+    ),
 
 }  # fmt: skip
 
