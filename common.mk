55f409947d1ce1cbd9657fdaf466fd84ac6a1e57
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-08-07 02:45:36 +0530

    sm6450-common: import media init scripts
    - Clean up .sh, remove unsupported soc
    - Apply https://github.com/moto-sm6450-dev/android_device_motorola_sm6450-common/commit/01ac85ebb072a433cde804e7ddf2e7851e24321f

--- common.mk
+++ common.mk
@@ -224,6 +224,10 @@ PRODUCT_PACKAGES += \
     
 # Media
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/init.qti.media.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.media.sh \
+    $(LOCAL_PATH)/media/init.qti.media.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.qti.media.rc
+
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/ravelin/media_codecs_performance_ravelin.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_ravelin.xml \
     $(LOCAL_PATH)/media/ravelin/media_codecs_ravelin.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_ravelin.xml \
     $(LOCAL_PATH)/media/ravelin/media_profiles_ravelin.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_ravelin.xml
